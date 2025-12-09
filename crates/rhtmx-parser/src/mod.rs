f4bd068a03fce5a5daa18e90b3dd02f1615b3f96
Jagjeet Singh <jeetkhinde@gmail.com>
2025-12-09 14:38:57 -0500

    Deleted MD files and rhtmx-parser crate

--- crates/rhtmx-parser/src/mod.rs
+++
@@ -1,8 +0,0 @@
-// File: src/parser/mod.rs
-// Purpose: Parse RHTML syntax and directives
-
-pub mod directive;
-pub mod expression;
-
-pub use directive::DirectiveParser;
-pub use expression::ExpressionEvaluator;
